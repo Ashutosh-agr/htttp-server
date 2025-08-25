4e1a0c2679aca3707f28d463f265fa87c2d354d1
Ashutosh Agarwal <ashutosh1503k@gmail.com>
2025-08-25 14:17:46 +0530

    error change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+package main.java;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-//public class Main {
+public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
